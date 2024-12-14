79c9de09670864e13948c813277b98b23deef4f1
Hmbilo <101592152+diliwo@users.noreply.github.com>
2024-12-14 16:05:11 +0100

    Containerized the web api

--- Services/AdminAreaManagement/AdminAreaManagement.Infrastructure/Persistence/DbContextExtensions/DbContextExtension.cs
+++ Services/AdminAreaManagement/AdminAreaManagement.Infrastructure/Persistence/DbContextExtensions/DbContextExtension.cs
@@ -26,8 +26,8 @@ namespace AdminAreaManagement.Infrastructure.Persistence.DbContextExtensions
         {
             if (!context.Teams.Any())
             {
-                //var path = Path.Combine("Persistence", "Seed", "Teams.json"); // uncomment When running in a docker container
-                string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/Teams.json"; // Uncomment when running locally
+                var path = Path.Combine("Persistence", "Seed", "Teams.json"); // uncomment When running in a docker container
+                //string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/Teams.json"; // Uncomment when running locally
 
                 var teams = JsonConvert.DeserializeObject<List<Team>>(File.ReadAllText(path));
 
@@ -40,8 +40,8 @@ namespace AdminAreaManagement.Infrastructure.Persistence.DbContextExtensions
 
             if (!context.Professions.Any())
             { 
-                //var path = Path.Combine("Persistence", "Seed", "Professions.json"); // uncomment When running in a docker container
-                string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/Professions.json"; // Uncomment when running locally
+                var path = Path.Combine("Persistence", "Seed", "Professions.json"); // uncomment When running in a docker container
+                //string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/Professions.json"; // Uncomment when running locally
 
                 var professions = JsonConvert.DeserializeObject<List<Profession>>(File.ReadAllText(path));
 
@@ -54,8 +54,8 @@ namespace AdminAreaManagement.Infrastructure.Persistence.DbContextExtensions
 
             if (!context.Schools.Any())
             {
-                //var path = Path.Combine("Persistence", "Seed", "Schools.json"); // uncomment When running in a docker container
-                string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/Schools.json"; // Uncomment when running locally
+                var path = Path.Combine("Persistence", "Seed", "Schools.json"); // uncomment When running in a docker container
+                //string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/Schools.json"; // Uncomment when running locally
 
                 var schools = JsonConvert.DeserializeObject<List<School>>(File.ReadAllText(path));
 
@@ -68,8 +68,8 @@ namespace AdminAreaManagement.Infrastructure.Persistence.DbContextExtensions
 
             if (!context.TrainingFields.Any())
             {
-                //var path = Path.Combine("Persistence", "Seed", "TrainingFields.json"); // uncomment When running in a docker container
-                string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/TrainingFields.json"; // Uncomment when running locally
+                var path = Path.Combine("Persistence", "Seed", "TrainingFields.json"); // uncomment When running in a docker container
+                //string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/TrainingFields.json"; // Uncomment when running locally
 
                 var trainingFields = JsonConvert.DeserializeObject<List<TrainingField>>(File.ReadAllText(path));
 
@@ -82,8 +82,8 @@ namespace AdminAreaManagement.Infrastructure.Persistence.DbContextExtensions
 
             if (!context.Trainings.Any())
             {
-                //var path = Path.Combine("Persistence", "Seed", "TrainingFields.json"); // uncomment When running in a docker container
-                string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/Trainings.json"; // Uncomment when running locally
+                var path = Path.Combine("Persistence", "Seed", "Trainings.json"); // uncomment When running in a docker container
+                //string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/Trainings.json"; // Uncomment when running locally
 
                 var Trainings = JsonConvert.DeserializeObject<List<Training>>(File.ReadAllText(path));
 
@@ -96,8 +96,8 @@ namespace AdminAreaManagement.Infrastructure.Persistence.DbContextExtensions
 
             if (!context.TrainingTypes.Any())
             {
-                //var path = Path.Combine("Persistence", "Seed", "TrainingTypes.json"); // uncomment When running in a docker container
-                string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/TrainingTypes.json"; // Uncomment when running locally
+                var path = Path.Combine("Persistence", "Seed", "TrainingTypes.json"); // uncomment When running in a docker container
+                //string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/TrainingTypes.json"; // Uncomment when running locally
 
                 var trainingTypes = JsonConvert.DeserializeObject<List<TrainingType>>(File.ReadAllText(path));
 
@@ -110,8 +110,8 @@ namespace AdminAreaManagement.Infrastructure.Persistence.DbContextExtensions
 
             if (!context.StaffMembers.Any())
             {
-                //var path = Path.Combine("Persistence", "Seed", "StaffMembers.json"); // uncomment When running in a docker container
-                string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/StaffMembers.json"; // Uncomment when running locally
+                var path = Path.Combine("Persistence", "Seed", "StaffMembers.json"); // uncomment When running in a docker container
+                //string path = "../adminareamanagement.infrastructure/persistence/seed/staffmembers.json"; // uncomment when running locally
 
                 var staffMembers = JsonConvert.DeserializeObject<List<StaffMember>>(File.ReadAllText(path));
 
@@ -122,19 +122,19 @@ namespace AdminAreaManagement.Infrastructure.Persistence.DbContextExtensions
                 }
             }
 
-            //if (!context.Partners.Any())
-            //{
-            //    //var path = Path.Combine("Persistence", "Seed", "Partners.json"); // uncomment When running in a docker container
-            //    string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/Partners.json"; // Uncomment when running locally
+            if (!context.Partners.Any())
+            {
+                var path = Path.Combine("Persistence", "Seed", "Partners.json"); // uncomment When running in a docker container
+                //string path = "../AdminAreaManagement.Infrastructure/Persistence/Seed/Partners.json"; // Uncomment when running locally
 
-            //    var partners = JsonConvert.DeserializeObject<List<Partner>>(File.ReadAllText(path));
+                var partners = JsonConvert.DeserializeObject<List<Partner>>(File.ReadAllText(path));
 
-            //    if (partners != null)
-            //    {
-            //        context.AddRange(partners);
-            //        context.SaveChanges();
-            //    }
-            //}
+                if (partners != null)
+                {
+                    context.AddRange(partners);
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
